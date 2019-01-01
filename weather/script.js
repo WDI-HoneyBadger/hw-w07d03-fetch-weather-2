50f992c0d9d91d524eca57df1fd98f823c7ee5c7
Layalkashgari <Layalkash@gmail.com>
2019-01-01 19:46:42 +0300

    doneee

--- weather/script.js
+++ weather/script.js
@@ -1,3 +1,42 @@
 $(document).ready(() => {
 
-}); 
+    console.log('jQ connected');
+
+    $('#submit').click(e => {
+        const num =  $('#zipcode').val();
+          fetchWeather(num);
+        });
+  
+    const fetchWeather = (zipCode) => {
+
+      const url = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&units=imperial&appid=0a1c6077653143999ddcc4382bcea8cf`
+  
+      fetch(url)
+      .then(response => response.json())
+      .then(data => {
+          console.log(data);
+          renderData(data);
+      })
+      .catch(error => console.log(error));
+    //     .then(response => response.json())
+    //     .then((data => { 
+    //         console.log(data)
+    //         renderData(data); 
+        
+    // })
+    //     .catch((error) => console.log(error))
+    // }
+    }
+
+
+ const renderData = (weather) => {
+
+    const $cityName = $('<h2>').text(weather.name).appendTo('#result');
+    const $temp = $('<h2>').text(weather.main.temp).appendTo('#result');
+    const $minTemp = $('<h2>').text(weather.main.temp_min).appendTo('#result');
+    const $maxTemp = $('<h2>').text(weather.main.temp_max).appendTo('#result');
+
+
+  }
+
+});
