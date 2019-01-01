4c92b969593722587d49ce75507fd8f2603168b1
Mohammed Alghamdi <44502915+MohammedAlghamdi3@users.noreply.github.com>
2019-01-02 00:46:03 +0300

    Add files via upload

--- weather/script.js
+++ weather/script.js
@@ -1,3 +1,38 @@
 $(document).ready(() => {
+    console.log('jQ Connected!');
+
+    $('#submit').click(() => {
+        $('#result').empty();
+        makecall($('#zipcode').val());
+    });
+
+    const makecall = (zipCode) => {
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${zipCode}&units=imperial&appid=a9bfdaae31fa1aa7f7e9d30d888b7ca7`;
+        fetch(url)
+            .then(response => response.json())
+            .then((data) => getData(data))
+            .catch(error => console.log(error));
+    }
+    const getData = (data) => {
+         city = data.name;
+         temp = data.main.temp;
+         description2 = data.weather[0].description;
+         min = data.main.temp_min;
+         max = data.main.temp_max;
+        appendToDom(city,temp,description2,min,max);
+    }
+
+
+    ///////////////////////
+    
+
+    const appendToDom = (city,temp,description2,min,max) => {
+        $('<h3>').text(`City  : ${city}`).appendTo('#result');
+        $('<h3>').text(`temperature : ${temp}`).appendTo('#result');
+         $('<h3>').text(`Weather description :  ${description2}`).appendTo('#result');
+         $('<h3>').text(`Min :  ${min}`).appendTo('#result');
+         $('<h3>').text(`Max :  ${max}`).appendTo('#result');
+    }
+
 
 }); 
