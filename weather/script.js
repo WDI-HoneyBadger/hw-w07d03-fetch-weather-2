997ef34fab1b6379af18c22b71e1e2ea9ab8266a
AlanoudBinSalamah <alanoud.binsalamah@gmail.com>
2019-01-02 08:49:13 +0300

    homework done

--- weather/script.js
+++ weather/script.js
@@ -1,3 +1,30 @@
 $(document).ready(() => {
 
+
+$('#submit').click(() =>{
+$('#result').empty();
+makeCall($('#zipcode')).val()
+
+
 }); 
+
+const makeCall=(zipcode)=>{
+    const url = 'http://api.openweathermap.org/data/2.5/weather?q=70124,us&units=imperial&appid=7375257e9cbb8c0b3aad72599de08284';
+    fetch(url)
+    .then(response=>response.json())
+    .then(data=>getData(data))
+    .catch(error=>console.log(error))
+
+}
+const getData=(zipcode)=>{
+const $name = $('<h2>').text('Name'+zipcode.name).append('#result')
+const $temp = $('<h2>').text('Temperature'+zipcode.main.temp).append('#result')
+const $description = $('<h2>').text('Description: '+zipcode.weather[0].description).appendTo('#result');
+const $minTemp = $('<h2>').text('MinTemp: '+zipcode.main.temp_min).appendTo('#result');
+ const $maxTemp = $('<h2>').text('MaxTemp: '+zipcode.main.temp_max).appendTo('#result');
+
+
+
+}
+
+})
