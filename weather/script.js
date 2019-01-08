49bec269fcb90922b68a7f7c654237a4b7aab486
abdulrabbt <abdulrab.bt@hotmail.com>
2019-01-08 18:28:34 +0300

    done

--- weather/script.js
+++ weather/script.js
@@ -19,11 +19,31 @@ $(document).ready(() => {
     };
 
     const hendleZipCode = (dataWeather) => {
-        const $cityName = $('<h2>').text(dataWeather.name).appendTo('#result');
-        const $temperature = $('<h2>').text(dataWeather.main.temp).appendTo('#result');
-        const $description = $('<h2>').text(dataWeather.weather[0].description).appendTo('#result');
-        const $mainTemp = $('<h2>').text(dataWeather.main.temp_min).appendTo('#result');
-        const $maxTemp = $('<h2>').text(dataWeather.main.temp_max).appendTo('#result');
+        const $cityName = $('<h2>').text('Name: '+dataWeather.name).appendTo('#result');
+        const $temperature = $('<h2>').text('temperature: '+dataWeather.main.temp).appendTo('#result');
+        if(dataWeather.main.temp >90){
+            $temperature.css('color','red')
+        } else if (dataWeather.main.temp < 40){
+            $temperature.css('color','blue')
+        }
+        const $description = $('<h2>').text('description: '+dataWeather.weather[0].description).appendTo('#result');
+        const $mainTemp = $('<h2>').text('main Temp: '+dataWeather.main.temp_min).appendTo('#result');
+        const $maxTemp = $('<h2>').text('max Temp: '+dataWeather.main.temp_max).appendTo('#result');
+        const $latitude = $('<h2>').text('latitude: '+dataWeather.coord.lat).appendTo('#result');
+        const $longitude = $('<h2>').text('longitude: '+dataWeather.coord.lon).appendTo('#result');
+        const $humidity = $('<h2>').text('humidity: '+dataWeather.main.humidity).appendTo('#result');
+        const $precipitation = $('<h2>').text('precipitation: '+dataWeather.base).appendTo('#result');
+        const $windSpeed = $('<h2>').text('windSpeed: '+dataWeather.wind.speed).appendTo('#result');
+        const $icon = $('<img>').appendTo('#result');
+        const $icon2 = $('<i>').addClass('fas fa-igloo').appendTo('#result');
+        $icon.attr('src', 'http://openweathermap.org/img/w/' + dataWeather.weather[0].icon + '.png');
+        const today = new Date()
+        const hours = today.getHours()
+
+        if (hours < 12)$('body,html').css('background-color','cornflowerblue')
+            else if (hours < 18)$('body,html').css('background-color','gold')
+                else $('body,html').css('background-color','NAVY')
+        
     }
 
 }); 
